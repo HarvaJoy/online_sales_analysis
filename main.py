81d73f8fbd452d3272875ea05ba4144c1fdf6659
Joyce Harvatovici <joyce.harvatovici@gmail.com>
2025-02-27 18:12:21 +0200

    Small changes on main.py, commented lines of code

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from data import products
 # Added products in Product manager
 product_manager = ProductManager()
 for product in products:
-    product_manager.add_product(Product(product["name"], product["price"], product["quantity"]))
+    product_manager.add_product(Product(
+                                product["name"], 
+                                product["price"], 
+                                product["quantity"]))
 
-# Display info about the products
-print("All Products:")
-print(product_manager.display_products())
+# # Display info about the products
+# print("All Products:")
+# print(product_manager.display_products())
 
-# Show the Total inventory value in stock
-print('Inventory Valuation: ', product_manager.calc_inventory_value())
+# # Show the Total inventory value in stock
+# print('Inventory Valuation: ', product_manager.calc_inventory_value())
 
 # Remove products by name
 print(product_manager.remove_product('Monitor'))
